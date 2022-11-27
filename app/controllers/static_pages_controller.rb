bab5e153f2904644bd99c8dbffceb277fc0ea10d
KeitaTakahashi1024 <b1017172@fun.ac.jp>
2022-11-27 20:47:30 +0900

    Add a Static Pages controller

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,8 @@
+class StaticPagesController < ApplicationController
+  # "rails generate controller StaticPages home help"を用いてStaticPagesコントローラを生成する
+  def home
+  end
+
+  def help
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  # 3.7 "rails generate controller StaticPages home help"を用いて生成する
+  get 'static_pages/home'
+  get 'static_pages/help'
   # 3.6 ルートルーティングを設定する
   root "application#hello"
 end
